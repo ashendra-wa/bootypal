b5ee564b1cf2c83b554e3bb866bca27162471a2a
ashendra-wa <amakashyap0246jploft@gmail.com>
2025-10-07 16:48:27 +0530

    add url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const serverApi = "https://api.bootypal.com";
+const serverApi = "http://52.27.106.190:3035";
+// "https://api.bootypal.com";
 // const serverApi = 'http://localhost:3001';
 export const backendUrl = "http://52.27.106.190:3035";
 export const baseUrl = serverApi;
